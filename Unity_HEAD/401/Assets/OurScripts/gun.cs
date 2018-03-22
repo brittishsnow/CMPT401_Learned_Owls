d0c828c40131537ed3997e4bfb1de9af46c42a38
JustinVanHeek <Justin.vanHeek@mytwu.ca>
2018-03-21 18:21:37 -0700

    First gun and target scripts

---
+++ Unity_HEAD/401/Assets/OurScripts/gun.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour {
+
+	public GameObject bullet;
+	public GameObject exitPoint;
+	public int bulletSpeed = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("Pressed left click.");
+			shoot ();
+		}
+
+		if (Input.GetMouseButtonDown (1)) {
+			Debug.Log ("Pressed right click.");
+		}
+
+		if (Input.GetMouseButtonDown (2)) {
+			Debug.Log ("Pressed middle click.");
+		}
+	}
+
+	private void shoot() {
+		Vector3 pos = gameObject.transform.position;
+		GameObject bulletInstance = GameObject.Instantiate (bullet, exitPoint.transform.position, gameObject.transform.rotation);
+		bulletInstance.GetComponent<Rigidbody> ().velocity = gameObject.transform.rotation * new Vector3 (-bulletSpeed,0,0);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Bullet striked something");
+		Destroy (gameObject);
+	}
+}
---
+++ Unity_HEAD/401/Assets/OurScripts/target.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour {
+
+	public bool correctTarget = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider bullet) {
+		if (bullet.CompareTag ("bullet")) {
+			if (correctTarget) {
+				correctAnswer ();
+			} else {
+				wrongAnswer ();
+			}
+		}
+	}
+
+	void correctAnswer() {
+		Debug.Log ("Correct!");
+	}
+
+	void wrongAnswer() {
+		Debug.Log ("Wrong!");
+	}
+}
