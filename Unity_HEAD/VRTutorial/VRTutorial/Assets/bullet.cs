78709f13d9cc5dedfcce6dd648a2540805559b99
Max Stow <mstow@me.com>
2018-04-04 15:28:05 -0700

    VR working ish with gun script

---
+++ Unity_HEAD/VRTutorial/VRTutorial/Assets/bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Bullet hit " + other.name);
+		other.SendMessage ("hit");
+		Destroy (gameObject);
+	}
+}
